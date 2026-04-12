57a8e6d20e4d99d532c8caa0eb82a0ea8470a5dd
jadhope50-max <jadhope50@gmail.com>
2026-04-12 14:18:29 -0400

    Update SecuritySystem.asm

--- SecuritySystem.asm
+++ SecuritySystem.asm
@@ -11,3 +11,25 @@ CH0:  DW 0
 
 ; I/O Constants
 Hex0:  EQU 004
+
+
+#####################
+ORG 0
+Reset:
+    IN   Channel0
+    OUT  Hex0
+    JUMP Reset
+
+Channel0:  EQU &HC0
+Channel1:  EQU &HC1
+Channel2:  EQU &HC2
+Channel3:  EQU &HC3
+Channel4:  EQU &HC4
+Channel5:  EQU &HC5
+Channel6:  EQU &HC6
+Channel7:  EQU &HC7
+Switches:  EQU 000
+LEDs:      EQU 001
+Timer:     EQU 002
+Hex0:      EQU 004
+Hex1:      EQU 005
