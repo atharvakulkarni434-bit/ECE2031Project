d342417475a02d89adbd2fe6974c4ad114884d08
jadhope50-max <jadhope50@gmail.com>
2026-04-12 14:19:53 -0400

    Update SecuritySystem.asm

--- SecuritySystem.asm
+++ SecuritySystem.asm
@@ -1,19 +1,4 @@
-; ADC Test - reads channel 0, displays on 7-seg
-ORG 0
-Reset:
-    IN   CH0
-    OUT  Hex0
-    JUMP Reset
 
-; ADC Peripheral Addresses
-ORG &HC0
-CH0:  DW 0
-
-; I/O Constants
-Hex0:  EQU 004
-
-
-#####################
 ORG 0
 Reset:
     IN   Channel0
